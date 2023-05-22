44a1db2f9c85795e48efae81d0881c8533def34d
jorgev259 <jorgev259@outlook.es>
2023-05-21 23:36:35 -0600

    Use modern next/image in SubmitButton

--- components/SubmitButton.jsx
+++ components/SubmitButton.jsx
@@ -1,14 +1,14 @@
 import { Button } from 'reactstrap'
 import classnames from 'classnames'
 import loader from 'svg-loaders/svg-smil-loaders/oval.svg'
-import Image from 'next/legacy/image'
+import Image from 'next/image'
 
 export default function SubmitButton (props) {
   const { loading = false, className, children, ...buttonProps } = props
 
   return (
     <Button {...buttonProps} type='submit' color='primary' className={classnames(className, { 'py-0': loading })} disabled={loading}>
-      {loading ? <Image alt='' {...loader} height={35} width={35} /> : children}
+      {loading ? <Image alt='' src={loader.src} height={35} width={35} /> : children}
     </Button>
   )
 }

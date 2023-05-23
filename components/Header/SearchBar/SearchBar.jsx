e67e9680054decf9433d2b137c5b76ff362fa4b7
jorgev259 <jorgev259@outlook.es>
2023-05-22 21:49:35 -0600

    Finalize search bar UI

--- components/Header/SearchBar/SearchBar.jsx
+++ components/Header/SearchBar/SearchBar.jsx
@@ -2,14 +2,16 @@
 import classNames from 'classnames'
 
 import styles from './SearchBar.module.scss'
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 export default function SearchBar () {
   const [open, setOpen] = useState(false)
+  const toggleShow = useCallback(() => setOpen(!open), [open])
 
   return (
-    <>
-      <div onClick={() => {}} className={classNames('fas fa-search me-2', styles.searchButton)} />
-    </>
+    <div className={classNames(styles.searchContainer, '', { [styles.open]: open })}>
+      {open ? <input className='mx-3'/> : null}
+      <div onClick={toggleShow} className={classNames('fas me-3', `fa-${open ? 'times' : 'search'}`, styles.searchButton)} />
+    </div>
   )
 }

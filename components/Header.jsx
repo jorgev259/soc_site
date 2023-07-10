d5ac637380faf7cd024ab659ec6f99248c02390d
jorgev259 <jorgev259@outlook.es>
2023-07-09 21:11:37 -0600

    whatever

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@ import styles from '../styles/Header.module.scss'
 
 import { useEffect, useState, useRef } from 'react'
 import Link from 'next/link'
-import Image from "next/legacy/image";
+import Image from 'next/legacy/image'
 import { Row, Col, Container, Button, Navbar, Nav, NavDropdown, Modal, Form } from 'react-bootstrap'
 import classNames from 'classnames'
 import { useRouter } from 'next/router'
@@ -201,11 +201,11 @@ function LoginButton (props) {
                     <SubmitButton loading={loadingLogin} type='submit' className='w-100' color='primary'>{t('Login')}</SubmitButton>
                   </Col>
                 </Row>
-                <Row className='mt-2'>
+                {/* <Row className='mt-2'>
                   <Col md={6} className='mx-auto'>
                     <Button onClick={() => setForgor(true)} className='w-100' color='primary'>{t('Recover password')}</Button>
                   </Col>
-                </Row>
+            </Row> */}
               </Form>
             )
           }
@@ -218,8 +218,8 @@ function LoginButton (props) {
 function RegisterProfileButton (props) {
   const { navMobile = false } = props
   const registerMutation = gql`
-    mutation ($username: String!, $email: String!, $pfp: Upload) {
-      registerUser(username: $username, email: $email, pfp: $pfp)
+    mutation ($username: String!, $email: String!, $password: String!, $pfp: Upload) {
+      registerUser(username: $username, email: $email, password: $password, pfp: $pfp)
     }
   `
 
@@ -229,7 +229,13 @@ function RegisterProfileButton (props) {
   const t = useTranslation()
   const [mutateRegister, { loading: loadingRegister }] = useMutation(registerMutation)
 
-  const submitRegister = e => {
+  const p1 = useRef(null)
+  const p2 = useRef(null)
+  const [isInvalid, setInvalid] = useState(false)
+
+  const checkInvalid = () => setInvalid(p1?.current?.value !== p2?.current?.value)
+
+  const submitRegister = async e => {
     e.persist()
     e.preventDefault()
 
@@ -298,6 +304,18 @@ function RegisterProfileButton (props) {
                   <Form.Group as={Col} >
                     <Form.Label htmlFor='email' style={{ color: 'black' }}>Email:</Form.Label>
                     <Form.Control required type='text' name='email' />
+                  </Form.Group>
+                </Row>
+                <Row className='mt-3'>
+                  <Form.Group as={Col} >
+                    <Form.Label htmlFor='password' style={{ color: 'black' }}>Password:</Form.Label>
+                    <Form.Control required type='password' name='password' isInvalid={isInvalid} ref={p1} onChange={checkInvalid} />
+                  </Form.Group>
+                </Row>
+                <Row className='mt-3'>
+                  <Form.Group as={Col} >
+                    <Form.Label htmlFor='passwordCheck' style={{ color: 'black' }}>Repeat password:</Form.Label>
+                    <Form.Control required type='password' name='passwordCheck' isInvalid={isInvalid} ref={p2} onChange={checkInvalid} />
                   </Form.Group>
                 </Row>
                 <Row className='mt-3'>

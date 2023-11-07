6ab9eea91791c9e52831cd843be600562050701b
jorgev259 <jorgev259@outlook.es>
2023-11-06 19:41:41 -0600

    Reconfigure server submodule

--- src/server/graphql/resolvers/index.js
+++
@@ -1,7 +0,0 @@
-import mutations from './mutations'
-import queries from './queries'
-import types from './types'
-
-const resolvers = { ...mutations, ...queries, ...types }
-
-export default resolvers

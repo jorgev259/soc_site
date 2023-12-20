40abf695266fff27955e4da99bbf963241edfea5
jorgev259 <jorgev259@outlook.es>
2023-12-19 20:30:04 -0600

    Add quality prop to AlbumBox components

--- components/AlbumBoxes.jsx
+++ components/AlbumBoxes.jsx
@@ -8,14 +8,14 @@ import styles from '@/styles/AlbumBoxes.module.scss'
 import { PLACEHOLDER, getImageUrl } from './utils'
 
 export default function AlbumBox (props) {
-  const { id, title, type = 'album', status, height = 300, width = 300, placeholder, style } = props
+  const { id, title, type = 'album', status, height = 300, width = 300, placeholder, style, quality } = props
   const coming = status === 'coming'
   const blurDataURL = placeholder || PLACEHOLDER
 
   const BoxContent = () => (
     <>
       <div className={styles.img}>
-        <Image alt={title}src={getImageUrl(id, type)} layout='responsive' width={width} height={height} placeholder='blur' blurDataURL={blurDataURL} />
+        <Image alt={title}src={getImageUrl(id, type)} layout='responsive' width={width} height={height} placeholder='blur' blurDataURL={blurDataURL} quality={quality} />
       </div>
       <div className='text-wrap text-center p-2'>
         {coming ? 'Coming Soon' : title}
@@ -38,11 +38,11 @@ export default function AlbumBox (props) {
 }
 
 export function AlbumBoxList (props) {
-  const { items, type, width, height, style, colProps = {} } = props
+  const { items, type, width, height, style, colProps = {}, quality } = props
 
   return items.map(albumProps => (
     <Col {...colProps} key={albumProps.id} className={classNames(styles.albumBoxContainer, styles[type], 'px-1 mb-2')}>
-      <AlbumBox {...albumProps} style={style} type={type} width={width} height={height}/>
+      <AlbumBox {...albumProps} style={style} type={type} width={width} height={height} quality={quality} />
     </Col>
   ))
 }

e93186bbfde9ba7d47e3f43e346ed4a5e7166982
jorgev259 <jorgev259@outlook.es>
2023-12-27 13:54:45 -0600

    SubmitButton as shared component

--- src/components/server/SubmitButton.jsx
+++ src/components/server/SubmitButton.jsx
@@ -1,20 +1,15 @@
 import classnames from 'classnames'
 
-const size = '20px'
+import Loading from './Loading'
 
 export default function SubmitButton (props) {
   const { loading = false, className, children, ...buttonProps } = props
 
   return (
     <button {...buttonProps} type='submit' className={classnames('btn btn-primary', className)} disabled={loading}>
-      {loading
-        ? (
-          <>
-            <span className="spinner-border spinner-border" aria-hidden="true" style={{ width: size, height: size }}></span>
-            <span className="visually-hidden" role="status">Loading...</span>
-          </>
-        )
-        : children}
+      <Loading>
+        {children}
+      </Loading>
     </button>
   )
 }

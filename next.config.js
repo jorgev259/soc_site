8087dcb5ae6435cc19b3870f16123b9d4d6fcb97
jorgev259 <jorgev259@outlook.es>
2023-12-28 17:08:26 -0600

    add 250 width images for use in OG tags

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withNextIntl = require('next-intl/plugin')('./i18n.js')
 module.exports = withNextIntl({
   eslint: { ignoreDuringBuilds: true },
   images: {
+    imageSizes: [16, 32, 48, 64, 96, 128, 250, 256, 384],
     remotePatterns: [
       { protocol: 'https', hostname: 'cdn.sittingonclouds.net', pathname: '/**' },
       { protocol: 'https', hostname: 'sittingonclouds.net', pathname: '/_next/image/**' }

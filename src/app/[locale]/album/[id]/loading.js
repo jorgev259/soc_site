4f591ac9042b181a330d5a5a8d41fa76fd775ec4
jorgev259 <jorgev259@outlook.es>
2023-12-29 21:35:59 -0600

    Visual tweaks to album page loading

--- src/app/[locale]/album/[id]/loading.js
+++ src/app/[locale]/album/[id]/loading.js
@@ -1,50 +1,38 @@
 import classNames from 'classnames'
-import Image from 'next/image'
 
 import styles from './AlbumPage.module.scss'
-import cover from '@/img/album/default.png'
-
-import { PLACEHOLDER } from '@/next/constants'
 
 export default function Loading () {
   return (
-    <>
-      <div className='row px-0 px-md-5'>
-        <div className='col col-12 col-lg-5 d-flex align-items-center px-lg-2 mb-3 mb-lg-0'>
-          <HeroCover />
-        </div>
-        <div className='col col-12 col-lg-7'>
-          <div className='blackBox'>
-            <div className='loadingAnim' style={{ height: '350px' }} />
-          </div>
-        </div>
-      </div>
-      <hr />
-      <div className='row'>
-        <div className={classNames('col col-12 col-lg-6', styles.trackList)}>
-          <div className='blackBox h-100 d-flex flex-column loadingAnim'>
-            <div className='row px-3 flex-grow-1 '>
-              <div className='col d-flex flex-column'>
-                <div style={{ height: '400px' }} />
-              </div>
+    <div className={classNames('row', styles.container)}>
+      <div className={classNames('col px-0 px-md-5 pt-3', styles.content, styles.loading)}>
+        <div className='row px-0 px-md-5'>
+          <div className='col col-12 col-lg-5 d-flex align-items-center px-lg-2 mb-3 mb-lg-0' />
+          <div className='col col-12 col-lg-7'>
+            <div className='blackBox'>
+              <div className='loadingAnim' style={{ height: '350px' }} />
             </div>
           </div>
         </div>
+        <hr />
+        <div className='row'>
+          <div className={classNames('col col-12 col-lg-6', styles.trackList)}>
+            <div className='blackBox h-100 d-flex flex-column loadingAnim'>
+              <div className='row px-3 flex-grow-1 '>
+                <div className='col d-flex flex-column'>
+                  <div style={{ height: '400px' }} />
+                </div>
+              </div>
+            </div>
+          </div>
 
-        <div className='col mt-3 mt-lg-0 col-12 col-lg-6'>
-          <div className='blackBox loadingAnim'>
-            <div style={{ height: '400px' }} />
+          <div className='col mt-3 mt-lg-0 col-12 col-lg-6'>
+            <div className='blackBox loadingAnim'>
+              <div style={{ height: '400px' }} />
+            </div>
           </div>
         </div>
       </div>
-    </>
-  )
-}
-
-function HeroCover () {
-  return (
-    <div className={styles.coverContainer}>
-      <Image quality={80} sizes ='40vw' className='rounded' fill alt='Album cover' src={cover} placeholder='blur' blurDataURL={PLACEHOLDER} />
     </div>
   )
 }

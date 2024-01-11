c0b731f7da74c7829eb72030b126218b82b5910f
jorgev259 <jorgev259@outlook.es>
2024-01-11 11:40:36 -0600

    WIP

--- next.config.js
+++ next.config.js
@@ -1,20 +1,27 @@
 const withNextIntl = require('next-intl/plugin')('./i18n.js')
+const { withPlugins, optional } = require('next-compose-plugins')
 
-module.exports = withNextIntl({
-  eslint: { ignoreDuringBuilds: true },
-  images: {
-    imageSizes: [16, 32, 48, 64, 96, 128, 250, 256, 384],
-    remotePatterns: [
-      { protocol: 'https', hostname: 'cdn.sittingonclouds.net', pathname: '/**' },
-      { protocol: 'https', hostname: 'sittingonclouds.net', pathname: '/_next/image/**' }
-    ]
-  },
-  webpack: (config, { isServer }) => {
-    config.module.rules.push({
-      test: /\.node$/,
-      loader: 'node-loader'
-    })
+module.exports = withPlugins(
+  [
+    [optional(() => require('@next/bundle-analyzer')), { enabled: true }, [process.env.ANALYZE === 'true']],
+    [withNextIntl]
+  ],
+  {
+    eslint: { ignoreDuringBuilds: true },
+    images: {
+      imageSizes: [16, 32, 48, 64, 96, 128, 250, 256, 384],
+      remotePatterns: [
+        { protocol: 'https', hostname: 'cdn.sittingonclouds.net', pathname: '/**' },
+        { protocol: 'https', hostname: 'sittingonclouds.net', pathname: '/_next/image/**' }
+      ]
+    },
+    webpack: (config, { isServer }) => {
+      config.module.rules.push({
+        test: /\.node$/,
+        loader: 'node-loader'
+      })
 
-    return config
+      return config
+    }
   }
-})
+)

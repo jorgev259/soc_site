a703fbe7e5a0fc4cd4a4031daeff647ac6b6a2bc
Jorge Vargas <jorgev259@outlook.es>
2024-08-02 11:54:43 -0600

    Homogenize error pages

--- pages/[locale]/404.jsx
+++ pages/[locale]/404.jsx
@@ -1,18 +1,28 @@
-import { Row, Col } from 'react-bootstrap'
-
-export default function Custom404 () {
+export default function Custom404() {
   return (
-    <Row className='h-100' style={{ backgroundColor: 'rgba(17,17,17,.7)' }}>
-      <Col className='h-100'>
-        <Row className='pt-4'>
-          <Col className='d-flex justify-content-center text-center'><span>Can you hear the bells ding dong..... because I can&#39;t</span></Col>
-        </Row>
-        <Row className='mt-4'>
-          <Col className='d-flex justify-content-center'>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/vHyGBWFOU-0" title="Perfection Can't Please Me" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-          </Col>
-        </Row>
-      </Col>
-    </Row>
+    <div className='row h-100' style={{ backgroundColor: 'rgba(17,17,17,.7)' }}>
+      <div className='col h-100'>
+        <div className='row pt-4'>
+          <div className='col d-flex justify-content-center text-center'>
+            <span>
+              Can you hear the bells ding dong..... because I can&#39;t
+            </span>
+          </div>
+        </div>
+        <div className='row mt-4'>
+          <div className='col d-flex justify-content-center'>
+            <iframe
+              width='560'
+              height='315'
+              src='https://www.youtube.com/embed/vHyGBWFOU-0'
+              title="Perfection Can't Please Me"
+              frameBorder='0'
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/app/[locale]/error.jsx
+++ src/app/[locale]/error.jsx
@@ -1,19 +1,26 @@
+'use client'
 import { YouTubeEmbed } from '@next/third-parties/google'
 
-export default function NotFound() {
+export default function Custom500() {
+  /*
+     ReactGA.exception({
+    description: 'An error ocurred',
+    fatal: true
+  })
+    */
   return (
     <div className='row h-100' style={{ backgroundColor: 'rgba(17,17,17,.7)' }}>
       <div className='col h-100'>
-        <div className='row pt-4'>
+        <div className='row pt-5'>
           <div className='col d-flex justify-content-center text-center'>
             <span>
-              Can you hear the bells ding dong..... because I can&#39;t
+              Something went wrong, but there&#39;s only going up from here!
             </span>
           </div>
         </div>
         <div className='row mt-4'>
           <div className='col d-flex justify-content-center'>
-            <YouTubeEmbed width={560} height={315} videoid='vHyGBWFOU-0' />
+            <YouTubeEmbed width={560} height={315} videoid='k4V3Mo61fJM' />
           </div>
         </div>
       </div>

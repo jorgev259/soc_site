5a3d4859a5c67d26d83bc7a11d35d4b2947b42c0
Jorge Vargas <jorgev259@outlook.es>
2024-08-28 20:17:07 -0600

    Add embed to webhook content

--- src/server/utils/requests.js
+++ src/server/utils/requests.js
@@ -1,31 +1,45 @@
 import axios from 'axios'
 
-function postWebhook (album, userText = '') {
+import { getNextCDNUrl } from '@/next/utils/getCDN'
+
+async function postWebhook(album, userText = '') {
   const url = `https://www.sittingonclouds.net/album/${album.id}`
   const content = `${url}${userText}`
-  const payload = { content }
+  const artists = await album.getArtists()
+  const embeds = [
+    {
+      title: album.title,
+      type: 'rich',
+      description: album.subTitle || artists.map((a) => a.name).join(' - '),
+      url,
+      color: album.headerColor,
+      thumbnail: {
+        url: getNextCDNUrl(album.id, 'album')
+      }
+    }
+  ]
+  const payload = { content, embeds }
 
-  axios.post(process.env.WEBHOOK_URL, payload)
-    .catch(err => console.log(err))
+  axios.post(process.env.WEBHOOK_URL, payload).catch((err) => console.log(err))
 }
 
 export const requestPOST = (operation, body) =>
   axios.post(`http://localhost:${process.env.REQUESTPORT}/${operation}`, body)
 
-export async function handleComplete (db, data, album) {
+export async function handleComplete(db, data, album) {
   if (data.request) {
-    db.models.request.findByPk(data.request)
-      .then(async request => {
-        if (request.state === 'complete') return
+    db.models.request.findByPk(data.request).then(async (request) => {
+      if (request.state === 'complete') return
 
-        await requestPOST('complete', { requestId: request.id })
+      await requestPOST('complete', { requestId: request.id })
 
-        const userText = request.userID || request.user
+      const userText =
+        request.userID || request.user
           ? ` ${request.userID ? `<@${request.userID}>` : `@${request.user}`} :arrow_down:`
           : ''
 
-        postWebhook(album, userText)
-      })
+      postWebhook(album, userText)
+    })
   } else {
     postWebhook(album)
   }
